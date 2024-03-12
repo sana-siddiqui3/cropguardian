3b889190d86e678ae1f0c063fe4314d3c2164616
Sana <sana-siddiqui@hotmail.co.uk>
2024-03-12 01:22:43 +0000

    commit

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function capitalizeFirstLetter(str) {
@@ -9,24 +9,18 @@ function capitalizeFirstLetter(str) {
 const Weather = () => {
     const [city, setCity] = useState('');
     const [weatherData, setWeatherData] = useState(null);
-    const [mapUrl, setMapUrl] = useState('');
     const fetchData = async () => {
         try {
             const response = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=88d6094d2110332d5e287f88c6ceea6f`
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=7de782c975d88bc1d52483ddf682d5e3`
         );
         setWeatherData(response.data);
-        setMapUrl(
-            `https://tile.openweathermap.org/map/rain_new/2/1/1.png?appid=88d6094d2110332d5e287f88c6ceea6f` // Replace YOUR_API_KEY with your actual API key
-          );
         console.log(response.data); //You can see all the weather data in console log
         } catch (error) {
              console.error(error);
         }
     };
-    useEffect(() => {
-        fetchData();
-    }, []);
+
     const handleInputChange = (e) => {
         setCity(e.target.value);
     };
@@ -57,12 +51,12 @@ const Weather = () => {
         <div className="rain">{Math.round(weatherData.rain)}mm</div>
         <div className="wind">{Math.round(weatherData.wind.speed)}m/s</div> 
         </div>
-        <img src={mapUrl} alt="Weather Map" style={{ width: '100%', marginTop: '10px' }} />
-
         </>
         ) : (
             <h2>&nbsp;</h2>
         )}
+
+        
      </div>
     );
 };
